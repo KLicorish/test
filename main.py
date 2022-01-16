d30e7619b3f15fd07376d53b17a4694290788460
KLicorish <b_licorish@yahoo.com>
2022-01-15 22:51:41 -0800

    Update main.py
    'nother test

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 
-print('lets see if this works!!!!')
+print('lets try one more thing')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
